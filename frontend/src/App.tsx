7c855ceabe89ede362c301c0f56cd94b63514049
Tamojit Das <tamojitdas181007@gmail.com>
2025-01-19 20:46:02 +0530

    Routing added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
-
 import SignupForm from './signup';
 import Login from './login';
 import StoreManagement from './store';
 import WarehouseManagement from './warehouse';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    
-    
-   {/* <Login /> */}
-  {/* < SignupForm /> */}
-  <StoreManagement />
-  {/* <WarehouseManagement/> */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/store" element={<StoreManagement />} />
+        <Route path="/warehouse" element={<WarehouseManagement />} />
+      </Routes>
     </>
   )
 }
